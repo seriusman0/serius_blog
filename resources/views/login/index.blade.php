e9f66956a691940db7fed1c7b4e63b7bcdc0ca74
serius <seriusman0@gmail.com>
2022-03-25 00:51:06 +0800

    dkfajkdlfja

--- resources/views/login/index.blade.php
+++ resources/views/login/index.blade.php
@@ -2,45 +2,46 @@
 
 @section('container')
 <div class="row justify-content-center">
-        <div class="col-md-4">
-         @if(session()->has('success'))
-          {{-- ALERT --}}
-          <div class="alert alert-success alert-dismissible fade show" role="alert">
-            {{ session('success')  }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-
-        @if(session()->has('loginError'))
-          {{-- ALERT --}}
-          <div class="alert alert-danger alert-dismissible fade show" role="alert">
-            {{ session('loginError')  }}
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-        </div>
-        @endif
-
-          <main class="form-signin">
-          <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
-            <form action="/login" method="post">
-              @csrf
-              <div class="form-floating">
-                <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email" placeholder="name@example.com" value="{{ old('email')  }}" autofocus required>
-                <label for="email">Email address</label>
-                @error('email')
-                  <div class="invalid-feedback">
-                      {{ $message  }}   
-                  </div> 
-                @enderror
-              </div>
-              <div class="form-floating">
-                <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
-                <label for="password">Password</label>
-              </div>
-          
-             <button class="w-100 btn btn-lg btn-primary" name="submit" type="submit">Login</button>
-            </form>
-            <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register Now!</a></small>
-          </main>
-        </div>
+  <div class="col-md-4">
+    @if(session()->has('success'))
+    {{-- ALERT --}}
+    <div class="alert alert-success alert-dismissible fade show" role="alert">
+      {{ session('success') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>
+    @endif
+
+    @if(session()->has('loginError'))
+    {{-- ALERT --}}
+    <div class="alert alert-danger alert-dismissible fade show" role="alert">
+      {{ session('loginError') }}
+      <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+    </div>
+    @endif
+
+    <main class="form-signin">
+      <h1 class="h3 mb-3 fw-normal text-center">Please Login</h1>
+      <form action="/login" method="post">
+        @csrf
+        <div class="form-floating">
+          <input type="email" name="email" class="form-control @error('email') is-invalid @enderror" id="email"
+            placeholder="name@example.com" value="{{ old('email')  }}" autofocus required>
+          <label for="email">Email address</label>
+          @error('email')
+          <div class="invalid-feedback">
+            {{ $message }}
+          </div>
+          @enderror
+        </div>
+        <div class="form-floating">
+          <input type="password" name="password" class="form-control" id="password" placeholder="Password" required>
+          <label for="password">Password</label>
+        </div>
+
+        <button class="w-100 btn btn-lg btn-primary" name="submit" type="submit">Login</button>
+      </form>
+      <small class="d-block text-center mt-3">Not Registered? <a href="/register">Register Now!</a></small>
+    </main>
+  </div>
+</div>
 @endsection
